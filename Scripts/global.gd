47fb886de69d05c6d1c01cf8e7535733f517b459
Gustavo Boer <Gustavo.igorsb@outlook.com>
2025-06-03 23:56:21 -0300

    Added eyes sprites and scene, some changes in global and UI

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -1,1 +1,24 @@
 extends Node
+
+signal too_lawful
+signal no_lawful
+signal lawful_changed
+
+var lawful := 100
+
+func change_lawful(amount : int):
+	lawful += amount
+	if amount >= 200:
+		set_lawful(200)
+		too_lawful.emit()
+		lawful_changed.emit()
+	
+	if amount <= 0:
+		set_lawful(0)
+		no_lawful.emit()
+		lawful_changed.emit()
+	
+	lawful_changed.emit()
+
+func set_lawful(amount : int):
+	lawful = amount
